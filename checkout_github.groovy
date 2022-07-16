0a320bffb92ff6d3d8b4f4f9a70affb5f43e5b16
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2022-07-16 14:04:45 +0100

    Create checkout_github.groovy

---
+++ checkout_github.groovy
@@ -0,0 +1,62 @@
+node('lsf-codon-submitter') {
+
+  stage('git version') {
+    sh 'git --version'
+  }
+
+  stage('checkout github') {
+
+    // checkout github repo
+    checkout(changelog: false,
+      poll: false,
+      scm: [$class: 'GitSCM',
+        branches: [
+          [name: '*/master']
+        ],
+        extensions: [
+          [$class: 'SubmoduleOption',
+            depth: 1,
+            disableSubmodules: false,
+            parentCredentials: false,
+            recursiveSubmodules: false,
+            reference: '',
+            shallow: true,
+            trackingSubmodules: false
+          ]
+        ],
+        gitTool: 'Default',
+        userRemoteConfigs: [
+          [url: 'https://github.com/pmb59/bioconductor-ci-validator']
+        ]
+      ])
+
+    sh '''#!/bin/bash
+    echo $WORKSPACE
+    mkdir subdir_repo
+    '''
+  }
+
+  // checkout gitlab private repo in a subdirectory
+  stage('checkout gitlab') {
+
+    dir('subdir_repo') {
+
+      checkout(changelog: false,
+        poll: false,
+        scm: [
+          $class: 'GitSCM',
+          branches: [
+            [name: '*/master']
+          ],
+          extensions: [],
+          userRemoteConfigs: [
+            [
+              credentialsId: '<CREDENTIALS>',
+              url: 'ssh://git@gitlab.<REPO>.git'
+            ]
+          ]
+        ])
+    }
+
+  }
+}
